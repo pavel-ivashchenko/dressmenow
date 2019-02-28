77fb3d1fef352c1942bc68d804542636d992b8f2
pasha <pavel.ivashchenko@itrexgroup.com>
2019-02-28 13:07:04 +0200

    Feature: slider is implemented

--- src/app/shared/slider/slider.component.ts
+++ src/app/shared/slider/slider.component.ts
@@ -1,18 +1,26 @@
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { SliderConf } from '@app/core/interfaces';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SliderComponent implements OnInit {
 
   @Input() sliderConf: SliderConf[];
+  public currSlide: SliderConf;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.setInitSlide();
+  }
+
+  public setInitSlide(): void {
+    this.currSlide = this.sliderConf[0];
+  }
 
 }

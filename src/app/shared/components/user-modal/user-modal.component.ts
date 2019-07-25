f7eb78d8104afc3c90842498b756611c6fc20058
Pavel Ivashchenko <pavel.ivashchenko@itrexgroup.com>
2019-07-25 10:53:41 +0300

    Feature(user modal): additional blocks` styles adjusted, mat inputs partially customized

--- src/app/shared/components/user-modal/user-modal.component.ts
+++ src/app/shared/components/user-modal/user-modal.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-user-modal',
   templateUrl: './user-modal.component.html',
-  styleUrls: ['./user-modal.component.scss']
+  styleUrls: ['./user-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserModalComponent implements OnInit {
 

e600e17fe58c47417d270bec263b13a14271b65e
pasha <pavel.ivashchenko@itrexgroup.com>
2019-10-16 14:49:26 +0300

    Feature(myaccount): wish list markup implemented

--- src/app/my-account/pages/my-account-home/my-account-home.component.ts
+++ src/app/my-account/pages/my-account-home/my-account-home.component.ts
@@ -10,6 +10,44 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 export class MyAccountHomeComponent implements OnInit {
 
   public isLoading = false;
+  public wishes = [
+    {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4287110_588472.jpg',
+      title: 'list #1'
+    }, {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4287110_588472.jpg',
+      title: 'list #2'
+    }
+  ];
+  public loves = [
+    {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4446470_390892.jpg'
+    }, {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4196710_844002.jpg'
+    }
+  ];
+  public purchases = [
+    {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4446470_390892.jpg'
+    }, {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4196710_844002.jpg'
+    }
+  ];
+  public orders = [
+    {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4446470_390892.jpg'
+    }, {
+      href: '#',
+      src: 'https://www.lulus.com/images/product/small-medium/4196710_844002.jpg'
+    }
+  ];
 
   constructor() { }
 

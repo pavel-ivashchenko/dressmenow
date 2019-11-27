7b0bbea481061b871a56a77a6674548740791e1b
pasha <pavel.ivashchenko@itrexgroup.com>
2019-11-27 16:31:54 +0200

    Fix(circle ci): configuration

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -4,7 +4,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu']
+    args: ['--no-sandbox']
   }
 };
 
